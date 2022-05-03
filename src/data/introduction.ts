ccf0e709e9ab1c865089044fb81b6cc8a39876ef
bkotikov <boggikotikov@gmail.com>
2022-05-04 00:02:48 +0300

    Einleitung

--- src/data/introduction.ts
+++ src/data/introduction.ts
@@ -32,3 +32,54 @@ export const introduction = {
         }
     ]
 }
+
+export const intro = {
+    roboterName: 'Jouiles',
+    emailImage: 'src/images/introduction/email.svg',
+    cityImage: 'src/images/introduction/city.svg',
+    cloudImage: 'src/images/introduction/cloud.svg',
+    roboterImages: [
+        {
+            src: 'src/images/introduction/roboterFirst.svg',
+            left: false,
+            right: true,
+            emailVisible: false,
+            cityVisible: false
+        },
+        {
+            src: 'src/images/introduction/roboterSecond.svg',
+            left: true,
+            right: false,
+            emailVisible: false,
+            cityVisible: false
+        },
+        {
+            src: 'src/images/introduction/roboterThird.svg',
+            left: true,
+            right: false,
+            emailVisible: false,
+            cityVisible: false
+        },
+        {
+            src: 'src/images/introduction/roboterFourth.svg',
+            left: true,
+            right: false,
+            emailVisible: false,
+            cityVisible: true
+        },
+        {
+            src: 'src/images/introduction/roboterFourth.svg',
+            left: true,
+            right: false,
+            emailVisible: true,
+            cityVisible: false
+        },
+        {
+            src: 'src/images/introduction/roboterSixth.svg',
+            left: true,
+            right: false,
+            emailVisible: false,
+            cityVisible: true
+        }
+    ]
+}

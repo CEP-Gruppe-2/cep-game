4abd02225034065eecc7733efc394171ca66f960
bkotikov <boggikotikov@gmail.com>
2022-05-09 16:07:27 +0300

    module json

---
+++ src/data/moduls.ts
@@ -0,0 +1,32 @@
+export const moduls = {
+    modul_eins: {
+        spiele: [
+            {
+                spielTitel: "",
+                spiellBeschreibung: "",
+                spielLink: "",
+                spielLinkUrl: "#" // fängt mit # an
+            }
+        ]
+    },
+    modul_zwei: {
+        spiele: [
+            {
+                spielTitel: "",
+                spiellBeschreibung: "",
+                spielLink: "",
+                spielLinkUrl: "#" // fängt mit # an
+            }
+        ]
+    },
+    modul_drei: {
+        spiele: [
+            {
+                spielTitel: "",
+                spiellBeschreibung: "",
+                spielLink: "",
+                spielLinkUrl: "#" // fängt mit # an
+            }
+        ]
+    }
+}

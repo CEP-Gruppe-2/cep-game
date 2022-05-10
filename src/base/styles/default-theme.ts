5941ae799435346b48bec8b8b4b6bf7f740977f8
bkotikov <boggikotikov@gmail.com>
2022-05-10 15:52:37 +0300

    project

---
+++ src/base/styles/default-theme.ts
@@ -0,0 +1,96 @@
+import { css } from 'lit';
+
+// Define your Theme
+let defaultTheme = css`
+	.dark {
+		--color-primary: #2cb5e8;
+		--color-primary-medium: #2cb5e8b0;
+		--color-primary-light: #2cb5e82b;
+        --color-primary-hover: #31c6fc;
+		--color-on-primary: #fff;
+		--color-app: linear-gradient(90deg, #0fb8ad 0%, #1fc8db 51%, #2cb5e8 75%);
+
+		--color-secondary: #0fb8ad;
+		--color-on-secondary: #fff;
+
+		/* Fondos */
+		--color-background: #111e23;
+		/* Fondos Textos que aparecen en los fondos */
+		--color-on-background: #fff;
+
+		/* Fondos que estan en cima de los fondos (ct-cards) */
+		--color-surface: #1a2c34;
+		/* Fondos Textos que aparecen en los ct-cards */
+		--color-on-surface: #fff;
+
+		--color-error: #cf6679;
+		--color-on-error: #fff;
+
+		--high-emphasis: #ffffffde;
+		--medium-emphasis: #ffffff99;
+		--color-disable: #ffffff61;
+
+		--color-borders: #bbbbbb24; /* divisores */
+	}
+
+	.light {
+		--color-primary: #2cb5e8;
+		--color-primary-medium: #2cb5e8b0;
+		--color-primary-light: #2cb5e82b;
+        --color-primary-hover: #31c6fc;
+		--color-on-primary: #fff;
+		--color-app: linear-gradient(90deg, #0fb8ad 0%, #1fc8db 51%, #2cb5e8 75%);
+
+		--color-secondary: #0fb8ad;
+		/* Color de objeto en cima de color de acento */
+		--color-on-secondary: #fff;
+
+		/* Fondos */
+		--color-background: #f7f7f8;
+		/* Fondos Textos que aparecen en los fondos */
+		--color-on-background: #535353; /* Gris */
+
+		/* Fondos que estan en cima de los fondos (ct-cards) */
+		--color-surface: #ffffff;
+		/* Fondos Textos que aparecen en los ct-cards */
+		--color-on-surface: #535353; /* Gris */
+
+		/* Color de objeto en cima de error */
+		--color-error: #b10808;
+		--color-on-error: #fff;
+
+		--high-emphasis: #000000de;
+		--medium-emphasis: #00000099;
+		--color-disable: #00000047;
+
+		--color-borders: #7c7c7c30;
+	}
+`;
+
+/**
+ * Inject CSS
+ */
+export function injectTheme() {
+	const link = document.createElement('link');
+	link.rel = 'stylesheet';
+	link.type = 'text/css';
+	link.href = 'https://fonts.googleapis.com/css?family=Roboto:400,500,700';
+	document.head.appendChild(link);
+	const style = document.createElement('style');
+	style.innerHTML = defaultTheme.cssText;
+	document.head.appendChild(style);
+}
+
+export class Theme {
+	static setTheme(color: 'dark' | 'light') {
+		if (color == 'dark') {
+			localStorage.theme = 'dark';
+			document.documentElement.classList.add('dark');
+			document.documentElement.classList.remove('light');
+		} else {
+			localStorage.theme = 'light';
+			document.documentElement.classList.remove('dark');
+			document.documentElement.classList.add('light');
+		}
+	}
+}
---
+++ vite.config.ts
@@ -0,0 +1,83 @@
+import { RollupBabelInputPluginOptions, babel } from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import copy, { CopyOptions } from 'rollup-plugin-copy';
+import minifyHTML from 'rollup-plugin-minify-html-literals';
+import summary from 'rollup-plugin-summary';
+import { terser } from 'rollup-plugin-terser';
+import { defineConfig } from 'vite';
+
+import app from './package.json';
+
+const babelConfig: RollupBabelInputPluginOptions = {
+	babelHelpers: 'bundled',
+	plugins: [
+		['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
+		'@babel/plugin-syntax-dynamic-import',
+		'@babel/plugin-syntax-import-meta',
+		'@babel/plugin-proposal-export-default-from'
+	],
+	babelrc: false,
+	presets: [['@babel/preset-env', { targets: { chrome: '72' }, debug: true }]]
+};
+
+const copyConfig: CopyOptions = {
+	hook: 'closeBundle',
+	targets: [
+        { src: 'node_modules/@webcomponents/webcomponentsjs/*', dest: 'dist/node_modules/@webcomponents/webcomponentsjs' },
+		{ src: 'public/index.universal.html', dest: 'dist', rename: 'index.html' },
+		{ src: 'res', dest: 'dist' }
+	]
+};
+
+// https://vitejs.dev/config/
+export default (opts: { mode: 'production' | 'development'; command: 'build' | 'serve' }) => {
+	return defineConfig({
+		server: {
+			port: 8000
+		},
+		define: {
+			'process.env.NODE_ENV': JSON.stringify(opts.mode),
+			'process.env.VERSION': JSON.stringify(app.version)
+		},
+		plugins: [],
+		build: {
+			assetsInlineLimit: 100000,
+			rollupOptions: {
+				input: {
+					app: './src/lit-app.ts'
+				},
+				// input: ['src/my-element.ts'],
+				// Specifies two JS output configurations, modern and legacy, which the HTML plugin will
+				// automatically choose between; the legacy build is compiled to ES5
+				// and SystemJS modules
+				output: {
+					// Legacy JS bundles (ES5 compilation and SystemJS module output)
+					format: 'systemjs',
+					chunkFileNames: `${opts.mode == 'development' ? '[name].' : 'c.'}[hash].js`,
+					entryFileNames: '[name].bundle.js',
+					dir: 'dist'
+				},
+				plugins: [
+					// Minify HTML template literals
+					minifyHTML(),
+					babel(babelConfig),
+					// Resolve bare module specifiers to relative paths
+					resolve(),
+					// Copy res to dist folder
+					copy(copyConfig),
+					// Minify JS
+                    terser({
+                        format: {
+                            comments: false
+                        },
+                        compress: false,
+                        module: true
+                    }),
+					// Print bundle summary
+					summary({}) as any
+				],
+				preserveEntrySignatures: false
+			}
+		}
+	});
+};

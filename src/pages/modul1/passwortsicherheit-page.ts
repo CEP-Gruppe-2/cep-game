a5dd4ab46c6158643e923b40bd900151ab97f291
ph-i-l <81186791+ph-i-l@users.noreply.github.com>
2022-05-12 15:00:26 +0200

    Passwortsicherheit erstellt und modul1 pages unterordner erstellt

---
+++ src/pages/modul1/passwortsicherheit-page.ts
@@ -0,0 +1,11 @@
+import { html, LitElement } from "lit";
+import { customElement } from "lit/decorators";
+
+@customElement('passwortsicherheit-page')
+export class Passwortsicherheit extends LitElement{
+
+
+    render() {
+        return html``
+        }
+}

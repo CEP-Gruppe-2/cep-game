9ae23b0b2498d470a0ed115826623703e5994952
ph-i-l <81186791+ph-i-l@users.noreply.github.com>
2022-05-17 09:51:37 +0200

    modul 1 passwortsicherheit hinzugefügt

--- src/data/modul1/passwortsicherheit.ts
+++ src/data/modul1/passwortsicherheit.ts
@@ -4,28 +4,105 @@ export const passwortsicherheit={
     einfuehrung: [
         {
             backgroundcolor:'#68C382',
-            joules:'src/images/modul1/passwortsicherheit/einfuehrungFirst.svg',
-            text:'Hallo hier bin ich wieder. Gut das du mehr über Passwortsicherheit wissen willst. <br/>Denn sichere Passwörter sind nicht nur ein wichtiger Bestandteil um das Unternehmen vor Angreifern und Schwachstellen zu schützen, sondern sind auch ein fester und wichtiger Bestandteil des Alltags geworden.'
+            inhalt:'src/images/modul1/passwortsicherheit/einfuehrungFirst.svg',
+            text:'Hallo hier bin ich wieder. Gut das du mehr über Passwortsicherheit wissen willst. <br/>Denn sichere Passwörter sind nicht nur ein wichtiger Bestandteil um das Unternehmen vor Angreifern und Schwachstellen zu schützen, sondern sind auch ein fester und wichtiger Bestandteil des Alltags geworden.',
+            buttonText:``
         },
         {
             backgroundcolor:'#68C382',
-            joules:'src/images/modul1/passwortsicherheit/einfuehrungSecond.svg',
-           text:'Hier steht warum'
+            inhalt:'src/images/modul1/passwortsicherheit/einfuehrungSecond.svg',
+            text:'Hier steht warum',
+            buttonText:``
         },
         {
             backgroundcolor:'68C382',
-            joules:'src/images/modul1/passwortsicherheit/einfuehrungThird.svg',
-           text:'Beim erstellen eines sicheren Passworts ist es wichtig einige Regeln zu beachten.<br/> Diese lernst du im folgenden Spiel besser kennen.',
-            buttonText:''
+            inhalt:'src/images/modul1/passwortsicherheit/einfuehrungThird.svg',
+            text:'Beim erstellen eines sicheren Passworts ist es wichtig einige Regeln zu beachten.<br/> Diese lernst du im folgenden Spiel besser kennen.',
+            buttonText:"Los geht's!"
+        },{
+            backgroundcolor:'000000',
+            inhalt:'src/images/modul1/passwortsicherheit/einfuehrungFifth.svg',
+            text:'Wähle aus den angezeigten Passwörtern das sicherste Passwort für die unten genannten Dienste. Online-Shop, soziales Netwerk und Onlinebanking',
+            buttonText:'Starten'
+        }
+
+    ],
+    aufgabe1: [
+        {
+            frage:'src/images/modul1/passwortsicherheit/frageeins.svg',
+            lösung:'C',
+            text:'',
+
+            richtig: [
+                {
+                     text:'src/images/modul1/passwortsicherheit/frageeinsrichtigFirst.svg'   
+                }
+                
+            ],
+            falsch:[
+                {
+                    text:'src/images/modul1/passwortsicherheit/frageeinsfalschFirst.svg'   
+               }
+               
+            ]
+        
+        },
+        {
+            frage:'src/images/modul1/passwortsicherheit/fragezwei.svg',
+            lösung:'A',
+            text:'',
+
+            richtig: [
+                {
+                     text:'src/images/modul1/passwortsicherheit/fragezweirichtigFirst.svg'  
+                }
+            ],
+            falsch:[
+               {
+                   text:'src/images/modul1/passwortsicherheit/fragezweifalschFirst.svg' 
+               }
+            ]
+        },
+        {
+            frage:'src/images/modul1/passwortsicherheit/fragedrei.svg',
+            lösung:'C',
+            text:'',
+
+            richtig: [
+                {
+                     text:'src/images/modul1/passwortsicherheit/fragedreirichtigFirst.svg'   
+                }
+            ],
+            falsch:[
+                {
+                    text:'src/images/modul1/passwortsicherheit/fragedreifalschFirst.svg'   
+               }
+            ]
         }
     ],
-    aufgabe: [
-        {},
-        {},
-        {},
-        {}
+    einfuehrungAufgabeZwei: [
+        {
+            backgroundcolor:'#68C382',
+            inhalt:'src/images/modul1/passwortsicherheit/einfuehrungAufgabeZweiFirst.svg',
+            text:'So jetzt bis du an der Reihe um dein gelerntes Wissen kreativ umzusetzen. In der Folgenden aufgabe darfst du die jetzt sichere Passwörter ausdenken.',
+            buttonText:``
+        },
+        {
+            backgroundcolor:'#68C382',
+            inhalt:'src/images/modul1/passwortsicherheit/einfuehrungAufgabeZweiSecond.svg',
+            text:'Hier nochmal die wichtigsten Punkte zusammengefast!',
+            buttonText:`Weiter geht's!`
+        },
+        {
+            backgroundcolor:'68C382',
+            inhalt:'src/images/modul1/passwortsicherheit/einfuehrungAufgabeZweiThird.svg',
+            text:'',
+            buttonText:"Starten"
+        }
+    ],
+    aufgabe2: [
+        
     ]
-
 
 
 }
--- src/pages/modul1/passwortsicherheit-page.ts
+++ src/pages/modul1/passwortsicherheit-page.ts
@@ -1,71 +1,250 @@
 import { css, html, LitElement } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, property, query, state } from "lit/decorators.js";
 import { passwortsicherheit } from "../../data/modul1/passwortsicherheit";
 import '../../components/button/index';
 
 @customElement('passwortsicherheit-page')
 export class Passwortsicherheit extends LitElement{
-    private position:number=2;
     private backgroundcolor:String='';
-    private joules:String='';
+    private inhalt:String='';
     private alt:String='';
+    private buttonText:String='';
     private start:boolean=false;
+    private lösung:String='';
+    private richtig:boolean= false;
+    private erklärung:boolean=false;
+    private modi2:boolean=false;
+    private passwort:String="";
 
+
+    @property()
+    position = 0;
+    positionErklaerung=0;
+    fehlerMeldungPasswort="";
+
+    @state()
+    _submitEnabled = false;
+
+    @query('input')
+    _input!: HTMLInputElement;
+    
     static styles = css`
     :host{
-        position: absolute;
-            display: block;
-            width: 100vw;
-            height: 100vh;
-            overflow: hidden;
-            box-sizing: border-box;
+            
+
+            #buttons{
+                float: left;
+                width: 30%;
+                margin-right: 3.33333%;
+                padding: 20px;
+                background: #eee;
+                box-sizing: border-box;
+                display: flex;
+                justify-content: space-between;
+                padding-left: 30%;
+                padding-right: 30%;
+            }
+            
+          
     }
     `
     
     _handleClick(e:Event):void{
-        this.position++;
-        console.log("Position: "+this.position);
+        /*wenn im spielmodus mit multiple choice lösung überprüfen*/
+       spielLogikMultipleChoice: if(!this.modi2&&this.start){
+            if((e.target as HTMLDivElement).textContent==="weiterNachErklärung"){
+                this.erklärung=false;
+                break spielLogikMultipleChoice;
+            }
+            
 
-        if((e.target as HTMLDivElement).textContent=="Los geht's!"){
-            this.position=0;
+            if((e.target as HTMLDivElement).textContent===this.lösung){
+                this.richtig=true;
+            }else{
+                this.richtig=false;
+            }
+            this.erklärung=true;
+        }else if(this.start){
+            if((e.target as HTMLDivElement).textContent==="Passwort überprüfen!"){
+                this._bearbeitePasswortEingabe()
+                if(this.richtig)
+                console.log("JA");
+                    /*rufe neue seite auf*/
+                else
+                return;
+
+                
+            }
+
+
+
+        }
+
+        if((e.target as HTMLDivElement).textContent==="Starten"){
+            this.position=-1;
             this.start=true;
         }
+
+        
+        
+        /*wenn aufgabe falsch oder richtig gemacht wurde erklärung durchgehen und Eklärungsposition hochzählen, ansonsten normale position weitermachen*/
+        if(this.erklärung){
+            console.log("Position Erklärung: "+this.positionErklaerung);
+            /*positionerklärung wird woanders hochgezählt*/
+        }else{
+            this.position++;
+            console.log("Position: "+this.position);
+            if(this.positionErklaerung>0){
+                this.positionErklaerung=0;
+                console.log("Position Erklärung auf 0 gesetzt!");
+            }
+                
+        }
+       this.requestUpdate();
     }
 
+
+    _inputChanged(e: Event) {
+        this._submitEnabled = !!(e.target as HTMLInputElement).value;
+      }
+
+
+      _bearbeitePasswortEingabe(){
+                      /*   Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:  */
+        let pattern=  "^(?=.*[a-z])(?=.*[A-Z]).{8,}$";
+
+        this.passwort=this._input.value;
+        this._input.value = '';
+        this._submitEnabled = false;
+        console.log(this.richtig);
+        if(!this.passwort.match(pattern))
+            this.fehlerMeldungPasswort="Das Passwort: "+this.passwort+" ist zu unsicher.";
+        else 
+            this.erklärung=true;
+
+        
+    }
+
+
+
+    
+
     render() {
+        
         let displayPosition;
-        console.log("hier");
-       
-        /*if(this.maxLaenge==this.position){
-            console.log("Done");
-            return;
-        }*/
 
         /*einführung aus Json holen*/
-        if(!this.start&&this.position<passwortsicherheit.einfuehrung.length){
-            this.backgroundcolor= passwortsicherheit.einfuehrung[this.position].backgroundcolor;
-            this.joules=passwortsicherheit.einfuehrung[this.position].joules;
+        if(!this.start){
+            if(!this.modi2&&this.position<=passwortsicherheit.einfuehrung.length){
+            /*modi 1 einfuehrung*/
+            /*this.backgroundcolor= passwortsicherheit.einfuehrung[this.position].backgroundcolor;*/
+            this.inhalt=passwortsicherheit.einfuehrung[this.position].inhalt;
             this.alt=passwortsicherheit.einfuehrung[this.position].text;
-            console.log(this.joules);
-        if(this.position==2){
+            this.buttonText=passwortsicherheit.einfuehrung[this.position].buttonText;
+
+            }else if(this.modi2&&this.position<=passwortsicherheit.einfuehrungAufgabeZwei.length){
+            /*modi 2 einführung*/
+            this.inhalt=passwortsicherheit.einfuehrungAufgabeZwei[this.position].inhalt;
+            this.alt=passwortsicherheit.einfuehrungAufgabeZwei[this.position].text;
+            this.buttonText=passwortsicherheit.einfuehrungAufgabeZwei[this.position].buttonText;
+            
+        }
+
+        if(this.buttonText!=''){
             displayPosition=html`
-            <img position: "absolute" width="2000" height="700" src=${this.joules}  alt=${this.alt}/>
-            <div><button-component  id="starten-button" textButton="Los geht's!" @click="${this._handleClick}">Los geht's!</button-component></div>
+            <img position: "absolute" width="2000" height="700" srcset=${this.inhalt}  alt=${this.alt}/>
+            <div><button-component  id="starten-button" textButton=${this.buttonText} @click="${this._handleClick}">${this.buttonText}</button-component></div>
             `
         }else{
             displayPosition = html `
-            <img position: "absolute" width="2000" height="700" src=${this.joules} @click="${this._handleClick}" alt=${this.alt}/>
+            <img position: "absolute" width="2000" height="700" srcset=${this.inhalt} @click="${this._handleClick}" alt=${this.alt}/>
             `
         }
 
-        /*nach start geht es hier weiter*/
-        }else if(this.position<=passwortsicherheit.aufgabe.length){
+        
+        }else{/*nach start geht es hier weiter*/
 
+            /*ausgabe modi1 aufgabe und lösung*/
+            if(!this.modi2){
+                if(!this.erklärung){
+                    /* ausgabe aufgabenstellung*/
+                    console.log("ausgabe aufgabe an Position: "+this.position)
+                    this.inhalt=passwortsicherheit.aufgabe1[this.position].frage;
+                    this.lösung=passwortsicherheit.aufgabe1[this.position].lösung;
+                    this.alt=passwortsicherheit.aufgabe1[this.position].text;
+
+                    displayPosition = html`
+                        <img position: "absolute" width="2000" height="700" srcset=${this.inhalt}  alt=${this.alt}/>
+            
+                        <div class="buttons">
+                            <button-component id="antwort-button" textButton='A' @click="${this._handleClick}">A</button-component>
+                            <button-component id="antwort-button" textButton='B' @click="${this._handleClick}">B</button-component>
+                            <button-component id="antwort-button" textButton='C' @click="${this._handleClick}">C</button-component>
+                            <button-component id="antwort-button" textButton='D' @click="${this._handleClick}">D</button-component>
+                        </div>
+            
+                        `
+                }
+                else/*ausgabe Lösung*/
+                {
+
+                    if(this.richtig &&this.positionErklaerung<=passwortsicherheit.aufgabe1[this.position].richtig.length)
+                        this.inhalt=passwortsicherheit.aufgabe1[this.position].richtig[this.positionErklaerung].text;
+                    else if(this.positionErklaerung<=passwortsicherheit.aufgabe1[this.position].falsch.length)
+                        this.inhalt=passwortsicherheit.aufgabe1[this.position].falsch[this.positionErklaerung].text;
+                    else{
+                        this.erklärung=false;
+                    }
+                this.positionErklaerung++;
+                displayPosition = html`
+                        <img srcset=${this.inhalt} />
+                        <button-component type="submit" id="weiter" textButton='weiter' @click="${this._handleClick}">weiterNachErklärung</button-component>
+                `
+                
+                /*ab jetzt nur noch modi2 verwenden*/
+                if(this.position===passwortsicherheit.aufgabe1.length-1){
+                    this.modi2=true;
+                    this.start=false;
+                    this.erklärung=false;
+                    this.position=-1;
+                    this.positionErklaerung=0;
+                    }
+                    
+                
+                }
+
+
+                
+            }else{ /*hier wird die aufgabe für modi2 ausgegeben*/
+
+                /*ausgabe Aufgabenstellung*/
+                if(!this.erklärung){
+                    console.log("Modi 2 aufgabenstellung wird geladen")
+                    displayPosition = html`
+                            <label>Gib hier ein Passwort an: 
+                                <input id="passwort-input" @input=${this._inputChanged}/>
+                            </label>
+                            <div id="fehlermeldung-container">${this.fehlerMeldungPasswort}</div>
+                            <button-component id="passwort-button" textButton="Passwort überprüfen!" @click=${this._handleClick} .disabled=${!this._submitEnabled}>Passwort überprüfen!</button-component>
+                       
+                    `
+
+                }else{/*ausgabe Lösung*/
+                console.log("Modi 2 lösung wird geladen")
+                displayPosition = html`
+                    <h1>erklärung und ende kapitel</h1>
+                `
+                }
+
+
+
+            }
+           
         }
-
-        this.position++;
         return html`
         ${displayPosition}
         `
         }
+
+    
 }

77adf1e4fabdafba91062d0ad3e75a8e18296c47
Thanos <atha.ar@arcor.de>
2022-05-25 09:49:58 +0200

    scaffold

---
+++ scaffold/scaffoldLit.ts
@@ -0,0 +1,19 @@
+import { unsafeCSS, html, LitElement } from 'lit';
+import styles from './scaffold.scss?inline'
+
+export class Scaffold extends LitElement {
+
+  static styles = unsafeCSS(styles);
+
+  render() {
+    return html``;
+  }
+}
+
+customElements.define("my-scaffold", Scaffold);
+
+// declare global {
+//   interface HTMLElementTagNameMap {
+//     'my-scaffold': Scaffold
+//   }
+// }

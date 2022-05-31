7af5de6f7e48555e13185eeece8425df20f22619
ph-i-l <81186791+ph-i-l@users.noreply.github.com>
2022-05-31 12:04:19 +0200

    security incidents

--- src/pages/security-incidents/security-incidents.ts
+++ src/pages/security-incidents/security-incidents.ts
@@ -7,8 +7,12 @@ import sicherheitsvorfaelle from '../../data/firstModule/security-incidents.json
 
 export class SecurityIncidents extends LitElement {
 
-  private bild:String="";
+  private bild:string="";
+  private text:string="";
   private buttons:String[]=[];
+  private antworten:String[]=[];
+  private erklärung1:boolean=false;
+  private richtigerButton:string="";
 
   static styles = unsafeCSS(styles);
 
@@ -16,7 +20,8 @@ export class SecurityIncidents extends LitElement {
   
   @property({type: Number})
   position = 0;
-
+  @property({type: Number})
+  auswahl = -1;
 
   /*wird bei klick auf bild aufgerufen und zählt position hoch*/
   _handleClickBild():void{
@@ -31,15 +36,43 @@ export class SecurityIncidents extends LitElement {
 
   /* wird bei klick auf button aufgerufen
       e:Event wir benötigt um zu erfahren welcher button gedrückt wird*/
-  _handleClickButton(e:Event):void{
-      this.buttons.length=0;
-      this.position++;
+    _handleClickButton(e:Event):void{
+        if(!this.erklärung1){
+            this.buttons.length=0;
+            this.position++;
+         }
+        else
+        {
+            console.log("Schleife")
+            schleife:for(var i:number=0;i < this.buttons.length;i++){
+            if(this.buttons[i]==(e.target as HTMLDivElement).textContent){
+                this.auswahl=i;
+                console.log("treffer"+i)
+                break schleife;
+            }
+
+        }
+        console.log("Schleife verlassen")
+        
+      }
+      
       this.requestUpdate;
-  }
+    }
+
+    private ladeErklärungsDurchlauf():void{
+        console.log("Lade erklärung")
+        this.bild=sicherheitsvorfaelle.ablauf[this.position].antworten[this.auswahl].hintergrund;
+        this.text="";
+    }
+
 
   /*lädt pfad zu bild aus JSON*/
   private ladeBild():void{
-      this.bild=sicherheitsvorfaelle.ablauf[this.position].hintergrund;
+        this.bild=sicherheitsvorfaelle.ablauf[this.position].hintergrund;
+        this.text=sicherheitsvorfaelle.ablauf[this.position].text;
+        this.richtigerButton=sicherheitsvorfaelle.ablauf[this.position].richtigerButton;
+      
+
   }
 
   /*lädt button texte aus JSON und speichert sie ins "buttons" array wenn der Inhalt nicht leer ist*/
@@ -58,8 +91,11 @@ export class SecurityIncidents extends LitElement {
   private schreibeBild():any{
       if(this.buttons.length==0)
       return html`<img class="svg" srcset=${this.bild} @click=${this._handleClickBild} />`
-      else
+      else if(this.text==""||this.text=="Frage")
       return html`<img class="svg" srcset=${this.bild} />`
+      else 
+      return html`<h1>${this.text}</h1>`
+      
   }
 
   /*wenn buttons gebraucht werden, erstellt buttons als html 
@@ -92,11 +128,26 @@ export class SecurityIncidents extends LitElement {
 
   render() {
       console.log("render")
+      if(!this.erklärung1){
           this.ladeButtons();
           this.ladeBild();
-          
-          
+      } else {
+        console.log("render in erklärung wird geladen")
+          this.ladeErklärungsDurchlauf();
+          this.erklärung1=false;
+          this.buttons=[];
+          this.antworten=[];
+          this.auswahl=-1;
+      }
 
+
+          if(this.text=="Sicherheitsvorfall: was ist das?"||this.text=="Frage"){
+              console.log("Erklärung1=true")
+              this.erklärung1=true;
+          }
+            
+          
+          console.log("Bilds:" +this.bild)
           let display=this.schreibeBild();
           let buttons=this.schreibeButtons();
           

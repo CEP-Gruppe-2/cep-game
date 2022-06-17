ea3688504032b0f339876a0b9d82477a83b98b79
bkotikov <boggikotikov@gmail.com>
2022-06-17 19:33:58 +0300

    Points Funktionen dokumentiert und kommentiert

--- src/functions/points.ts
+++ src/functions/points.ts
@@ -1,15 +1,29 @@
 import { changeCookie, getCookie } from './cookies';
 
-const addPoints = (points : number) => {
-  points = points + parseInt(getPoints());
-  changeCookie({name: 'points', value: points.toString()});
+/**
+ * Save points into Cookie
+ * @param points {number} - Value of points
+ * @returns {void}
+ */
+const addPoints = (points : number) : void => {
+  points = points + parseInt(getPoints()); // get Points from Cookie, parse to Integer and add it to Points
+  changeCookie({name: 'points', value: points.toString()}); // call changeCookie Function and change old Cookie Value
 };
 
-const removePoints = (points : number) => {
+/**
+ * Remove points from Cookies
+ * @param points {number} - Value of points
+ * @returns {void}
+ */
+const removePoints = (points : number) : void => {
   points = points - parseInt(getPoints());
   changeCookie({name: 'points', value: points.toString()});
 };
 
+/**
+ * Return points from Cookies
+ * @returns {string}
+ */
 const getPoints = function () : string {
   return getCookie('points')!;
 };

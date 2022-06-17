55da46f8b79d27f9f58d2d773f9b0e026e0b3d06
bkotikov <boggikotikov@gmail.com>
2022-06-17 19:35:34 +0300

    Tests erstellt

---
+++ test/time/timeEqualTenMinutes.test.ts
@@ -0,0 +1,36 @@
+import {fakeLocalStorage} from '../../src/functions/fakeLocalstorage'
+import {getLastTimeExchangeMinutes, exchangePointsAndSetLastNextExchang} from './timeFunctions'
+
+describe('Zeitunterschied in Minuten', () => {
+
+    /*
+        Es generiert ein "fake" Localstorage
+    */
+    beforeAll(() => {
+        Object.defineProperty(window, 'localStorage', {
+        value: fakeLocalStorage,
+        });
+    });
+
+    /*
+        Ruft die "setItemLocalStorage" Methode auf und 
+        setzt Name und Value in der Localstorage
+    */
+    it('Vergleicht den Unterschied zw. zwei verschiedenen Zeiten in Minuten', () => {
+
+        /*
+            Hier wird die Funktion exchangePointsAndSetLastNextExchang() aufgerufen,
+            in der Funktion wird der lezte Zeitpunkt der letzten Speicherung erfasst und
+            um weitere 10 Minuten erweitert. Danach wird die Zeiten in der LocalStorage abgespeichert
+        */
+
+        exchangePointsAndSetLastNextExchang(10);
+        const lastTime = getLastTimeExchangeMinutes()
+        
+        /*
+             Hier wird der Unterschied zw. der letzten Speicherung (10 Punkte) verglichen
+             Da wir die Zeit um 10 Minuten erweitern haben, muss deer Unterschied genau 10 Minuten entsprechen
+        */
+        expect(10).toBe(lastTime)
+    });
+});
